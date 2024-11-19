5f14d00b4394cff024dd200ae3ad158497ef5236
Narayanan <narayanan@CLLAP-CHN-2576.local>
2024-11-19 14:57:00 +0530

    added camera launcher logics

--- app/src/main/java/com/example/mywhatsapp/MainActivity.kt
+++ app/src/main/java/com/example/mywhatsapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.mywhatsapp
 
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,13 +15,46 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.navigation.activity
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.mywhatsapp.ui.theme.MywhatsappTheme
+import kotlinx.coroutines.Dispatchers
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import kotlin.coroutines.CoroutineContext
+import android.Manifest
+import android.os.Build
+import androidx.annotation.RequiresApi
 
+class MainViewModelFactory(private val coroutineContext: CoroutineContext) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewmodel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MainViewmodel(coroutineContext) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
 class MainActivity : ComponentActivity() {
+    var photoFile: File? = null
+    val CAPTURE_IMAGE_REQUEST = 1
+    var mCurrentPhotoPath: String? = null
+
+    val viewModel: MainViewmodel by viewModels{
+        MainViewModelFactory(Dispatchers.Main)
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,7 +67,7 @@ class MainActivity : ComponentActivity() {
 
                    NavHost(
                        navController= navController,
-                       startDestination = BottomBarScreen.Status.route,
+                       startDestination = BottomBarScreen.Chat.route,
                        modifier = Modifier.padding(paddingValues)
                        ){
                        composable(BottomBarScreen.Status.route){
@@ -40,11 +77,12 @@ class MainActivity : ComponentActivity() {
                            Text("Hello Call")
                        }
                        composable(BottomBarScreen.Camera.route){
-                           Text("Hello Camera")
 
+                          AlbumScreen(modifier = Modifier,viewModel)
                        }
                        composable(BottomBarScreen.Chat.route){
-                           Text("Hello Chat")
+                           window.statusBarColor = resources.getColor(R.color.status_bar_color)
+                           ChatScreen()
                        }
                        composable(BottomBarScreen.Settings.route){
                            Text("Hello Settings")
@@ -55,4 +93,20 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+data class AlbumViewState(
+
+    val tempFileUrl: Uri? = null,
+
+    val selectedPictures: List<ImageBitmap> = emptyList(),
+)
+sealed class Intent {
+    data class OnPermissionGrantedWith(val compositionContext: Context): Intent()
+    data object OnPermissionDenied: Intent()
+    data class OnImageSavedWith (val compositionContext: Context): Intent()
+    data object OnImageSavingCanceled: Intent()
+    data class OnFinishPickingImagesWith(val compositionContext: Context, val imageUrls: List<Uri>): Intent()
+}
+
+
+
 
--- app/src/main/java/com/example/mywhatsapp/ui/Components.kt
+++ app/src/main/java/com/example/mywhatsapp/ui/Components.kt
@@ -1,7 +1,15 @@
 package com.example.mywhatsapp
 
 
+import android.content.pm.PackageManager
+import android.os.Build
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -9,24 +17,46 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridCells.*
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -87,3 +117,185 @@ fun CustomBottomNavigationBar(navController: NavHostController) {
         }
     }
 }
+
+
+@Composable
+fun ChatScreenItem(){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(Color.White),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Image(painter = painterResource(R.drawable.oval),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(vertical = 11.dp)
+                .padding(start = 16.dp,end=12.dp)
+                .size(52.dp)
+
+            )
+        Column(
+            modifier = Modifier
+                .weight(0.8f),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ){
+            Row {
+                Text(
+                    text = "Bathri Narayanan",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.weight(0.1f)
+
+                )
+                Text(
+                    text = "11/15/19",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    color = Color(0xFF8E8E93),
+                    modifier = Modifier
+
+                )
+            }
+            Text(
+                text = "Great things take time",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Normal,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                color = Color(0xFF8E8E93),
+                modifier = Modifier
+
+            )
+            Spacer(modifier = Modifier
+                .height(1.dp)
+                .fillMaxWidth()
+                .background(Color(0x3c3c43aa))
+                )
+        }
+
+        Image(
+            painter = painterResource(R.drawable.right_arrow),
+            contentDescription = null,
+            modifier = Modifier.size(width = 7.dp, height = 12.dp).weight(0.1f)
+        )
+    }
+}
+
+@Composable
+fun ChatTopBar(){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(Color(0xFFF6F6F6))
+            .padding(start = 16.dp, bottom = 12.dp, top = 12.dp, end =16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text("Edit", color = Color(0xFF007aff), fontSize = 17.sp)
+        Text("Chats", fontSize = 17.sp, fontWeight = FontWeight.SemiBold)
+        Image(painter = painterResource(R.drawable.edit_icon), contentDescription = null,
+            modifier = Modifier.size(24.dp))
+    }
+}
+
+
+@Composable
+fun ChatScreen(){
+    val scollstate = rememberScrollState()
+    Scaffold (
+        topBar = {
+            ChatTopBar()
+        },
+
+    ){
+        padding->
+        Column(modifier = Modifier.fillMaxSize().padding(padding).verticalScroll(scollstate)) {
+            for(i in 1..20){
+                ChatScreenItem()
+            }
+        }
+    }
+
+}
+@Composable
+@RequiresApi(Build.VERSION_CODES.P)
+fun AlbumScreen(modifier: Modifier = Modifier, viewModel: MainViewmodel) {
+
+    val currentContext = LocalContext.current
+    val viewState: AlbumViewState by viewModel.viewStateFlow.collectAsState()
+
+    val pickImageFromAlbumLauncher = rememberLauncherForActivityResult(ActivityResultContracts.PickMultipleVisualMedia()) { urls ->
+        viewModel.onReceive(Intent.OnFinishPickingImagesWith(currentContext, urls))
+    }
+
+    val cameraLauncher = rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) { isImageSaved ->
+        if (isImageSaved) {
+            viewModel.onReceive(Intent.OnImageSavedWith(currentContext))
+        } else {
+            // handle image saving error or cancellation
+            viewModel.onReceive(Intent.OnImageSavingCanceled)
+        }
+    }
+
+    // launches camera permissions
+    val permissionLauncher = rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { permissionGranted ->
+        if (permissionGranted) {
+            viewModel.onReceive(Intent.OnPermissionGrantedWith(currentContext))
+        } else {
+            // handle permission denied such as:
+            viewModel.onReceive(Intent.OnPermissionDenied)
+        }
+    }
+
+    LaunchedEffect(key1 = viewState.tempFileUrl) {
+        viewState.tempFileUrl?.let {
+            cameraLauncher.launch(it)
+        }
+    }
+
+    val context = LocalContext.current
+    val cameraPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            Toast.makeText(context, "Camera Permission Granted", Toast.LENGTH_SHORT).show()
+            // You can now open the camera or perform any camera-related action
+        } else {
+            Toast.makeText(context, "Camera Permission Denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+    val hasCameraPermission = ContextCompat.checkSelfPermission(
+        context,
+        android.Manifest.permission.CAMERA
+    ) == PackageManager.PERMISSION_GRANTED
+
+    Column(modifier = Modifier) {
+        Row {
+            Button(onClick = {
+                // get user's permission first to use camera
+               permissionLauncher.launch(android.Manifest.permission.CAMERA)
+
+            }) {
+                Text(text = "Take a photo")
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Button(onClick = {
+                // Image picker does not require special permissions and can be activated right away
+                val mediaRequest =
+                    PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                pickImageFromAlbumLauncher.launch(mediaRequest)
+            }) {
+                Text(text = "Pick a picture")
+            }
+        }
+
+
+    }
+    }
